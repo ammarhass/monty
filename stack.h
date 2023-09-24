42b0f18ae8f7f51a4e5fba8817ae99d80c4cd1f4
Super User <ammar.hass94@gmail.com>
2023-09-24 02:52:52 -0700

    Task 0

---
+++ stack.h
@@ -0,0 +1,58 @@
+#ifndef _STACK_H
+#define _STACK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct handle_argument - handle argument of opcode
+ *
+ * @num: string containg argument
+ * @textfile: pointer to the file
+ *
+ */
+typedef struct handle_argument
+{
+	char *num;
+	FILE *textfile;
+} handle_t;
+
+extern handle_t arg;
+int getline(char **line, size_t *n, FILE *textfile);
+void Push_To_Stack(stack_t **head, unsigned int n);
+void Print_Stack(stack_t **head, unsigned int line_num);
+void Top_Of_Stack(void);
+void pop(void);
+
+#endif
